7249eef0a0add1353b8a9e16ff3ea551f705c0ed
Your Name <you@your-domain.com>
2025-08-12 09:34:36 +0900

    Add feature for updating recipes.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -17,6 +17,21 @@ class TagSerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
         read_only_fields = ['id']
 
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipes."""
+    tags = TagSerializer(many=True, required=False)
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        read_only_fields = ['id']
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
+
     def _get_or_create_tags(self, tags, recipe):
         """Handle getting or creating tags as needed."""
         auth_user = self.context['request'].user
@@ -47,18 +62,3 @@ class TagSerializer(serializers.ModelSerializer):
 
         instance.save()
         return instance
-
-class RecipeSerializer(serializers.ModelSerializer):
-    """Serializer for recipes."""
-    tags = TagSerializer(many=True, required=False)
-
-    class Meta:
-        model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
-        read_only_fields = ['id']
-
-class RecipeDetailSerializer(RecipeSerializer):
-    """Serializer for recipe detail view."""
-
-    class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
